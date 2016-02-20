5a9cf3451e85a90430d8d60d32a03f27e34168ad
田代やよい <o.841.312@gmail.com>
2016-02-20 23:11:28 +0900

    initial commit

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,22 @@
+class CustomersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def upate
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
