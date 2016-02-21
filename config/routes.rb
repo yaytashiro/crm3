b2c82fe3103f727c103e36ccdb024a3e3cd4ece4
田代やよい <o.841.312@gmail.com>
2016-02-21 10:00:56 +0900

    エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  get 'customers/index'
-
-  get 'customers/show'
-
-  get 'customers/upate'
-
-  get 'customers/destroy'
-
-  get 'customers/create'
-
-  get 'customers/new'
-
-  get 'customers/edit'
+  resources :customers
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
